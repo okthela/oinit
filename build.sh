914ffeaae496bf468d3ea27c1496be37bcc0fdf7
okla14 <rensersuggestoes@gmail.com>
2026-02-23 18:41:43 -0300

    shutdown and reboot are now in one single file

--- build.sh
+++ build.sh
@@ -2,14 +2,12 @@
 
 echo "Building"
 echo init.nim
-nim c -d:danger -d:strip -d:lto --opt:speedinit.nim
+nim c -d:danger -d:strip -d:lto --opt:speed init.nim
 echo shutdown.nim  -d:danger
 nim c -d:danger -d:strip -d:lto --opt:speed shutdown.nim
-echo reboot.nim 
-nim c -d:danger -d:strip -d:lto --opt:speed reboot.nim 
+
 echo "Moving to /sbin/oinit"
 sudo cp init /sbin/oinit
-echo "Moving to /sbin/oinit-reboot"
-sudo cp reboot /sbin/oinit-reboot
+
 echo "Moving to /sbin/oinit-shutdown"
 sudo cp shutdown /sbin/oinit-shutdown
--- shutdown.nim
+++ shutdown.nim
@@ -1,21 +1,44 @@
-import osproc, posix
+import osproc, posix, os
 
 let whoisi = execProcess("/sbin/whoami")
 
-if whoisi == "root":
+let option = paramStr(1)
+
+if whoisi == "root\n":
     echo whoisi
 else:
+    echo whoisi
     echo "must be root"
     quit()
 
-echo "System going down for poweroff"
-echo "Syncing file systems"
-sync()
-echo "Remounting / as Read-Only"
-discard execProcess("/sbin/mount -o remount,ro /")
-echo "Bye-bye!"
+proc shutdown() =
+    let poweroff = "o"
+    writeFile("/proc/sysrq-trigger", poweroff)
+    echo "System going down for poweroff"
+    echo "Syncing file systems"
+    sync()
+    echo "Remounting / as Read-Only"
+    discard execProcess("/sbin/mount -o remount,ro /")
+    discard posix.sleep(1)
+    echo "Bye-bye!"
 
-discard sleep(1)
+proc reboot() =
+    let poweroff = "b"
+    echo "System going down for reboot"
+    echo "Syncing file systems"
+    sync()
+    echo "Remounting / as Read-Only"
+    discard execProcess("/sbin/mount -o remount,ro /")
+    echo "Bye-bye!"
+    discard posix.sleep(1)
+    writeFile("/proc/sysrq-trigger", poweroff)
 
-let poweroff = "o"
-writeFile("/proc/sysrq-trigger", poweroff)
+if option == "reboot":
+    echo "Rebooting"
+    reboot()
+elif option == "shutdown":
+    echo "Shutting down"
+    shutdown()
+else:
+    echo "unknown option"
+#writeFile("/proc/sysrq-trigger", poweroff)
